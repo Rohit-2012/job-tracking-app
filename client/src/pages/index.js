c65c8d0b3660f060e2ea041ecd69d003389c4de7
Rohit-2012 <rohit.kirti1219@gmail.com>
2024-04-23 16:21:45 +0530

    completed admin page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,7 +2,6 @@ export { default as AddJob } from "./AddJob";
 export { default as Admin } from "./Admin";
 export { default as AllJobs } from "./AllJobs";
 export { default as DashboardLayout } from "./DashboardLayout";
-export { default as DeleteJob } from "./DeleteJob";
 export { default as EditJob } from "./EditJob";
 export { default as Error } from "./Error";
 export { default as HomeLayout } from "./HomeLayout";
